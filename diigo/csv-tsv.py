849c2692d2d8782a7c175f41146080186c82cf78
deleyva <jesuslopezdeleyva@gmail.com>
2018-10-29 23:01:05 +0100

    csv arreglao

--- diigo/csv-tsv.py
+++ diigo/csv-tsv.py
@@ -1,6 +1,7 @@
 import csv
 from bs4 import BeautifulSoup
 import pprint
+import re
 
 filename = input('Introduce el nombre del archivo: ')
 lista_out = []
@@ -10,34 +11,33 @@ with open(filename,'r') as csvin:
     lista_csvin = list(csvin)
     print(len(lista_csvin))
     for item in lista_csvin[1:]:
-#        print(item)
-        lineas = iter(item[4].splitlines())
+        lineas = item[4].replace('\nSticky note: ', 'Sticky note: ')
+        lineas = lineas.splitlines()
         for linea in lineas:
-#            print('Linea :', linea)
-            if linea.startswith('Highlight: '):
-                campos = {}
-                campos['back'] = BeautifulSoup(linea[11:], 'html.parser').text
-                campos['title'] = item[0]
-                campos['url'] = item[1]
-                campos['tags'] = item[2]
-                campos['description'] = item[3]
-            elif linea.startswith('Sticky note: '):
-                campos['front'] = str(linea[13:])
-                lista_out.append(campos)
+            campos = {}
+            items = re.findall(r'Highlight: (.*)Sticky note: (.*)', linea)
+            if len(items) != 1:
+                items = re.findall(r'Highlight: (.*)', linea)
+                # print(items[0])
+                campos['back'] = BeautifulSoup(items[0], 'html.parser')
+                campos['front'] = ''
             else:
-                try:
-                    if next(lineas).startswith('Highlight: ') or next(lineas) == None:
-                        lista_out.append(campos)
-                except Exception as e:
-                    print(e)
-            print(campos)
+                # print(items[0][0], '\n', items[0][1])
+                campos['back'] = BeautifulSoup(items[0][0], 'html.parser')
+                campos['front'] = items[0][1]
+            campos['title'] = item[0]
+            campos['url'] = item[1]
+            campos['tags'] = item[2]
+            campos['description'] = item[3]
+            lista_out.append(campos)
+            
 
-#pprint.pprint(lista_out)
+pprint.pprint(lista_out)
 
-# with open('export.tsv', 'w') as tsvout:
-#     tsvout = csv.writer(tsvout, delimiter='\t')
-#     for item in lista_out:
-#         row = list(item.values())
-#         tsvout.writerow(row)
+with open('export.txt', 'w') as tsvout:
+    tsvout = csv.writer(tsvout, delimiter='\t')
+    for item in lista_out:
+        row = list(item.values())
+        tsvout.writerow(row)
 
 
